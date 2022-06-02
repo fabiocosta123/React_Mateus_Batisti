b5d805ad09c569904833b39020f60b9c07c01545
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2022-06-02 00:11:59 -0300

    Props e destruturando props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import ListRender from "./Components/ListRender";
-import ConditionalRender from "./Components/ConditionalRender";
+import ShowUserName from "./Components/ShowUserName";
+import CarDetails from "./Components/CarDetails";
 
 const App = () => {
   return (
     <div>
       <h1>Avançando em React</h1>
       <div>
-        <ListRender />
-        <ConditionalRender />
+        {/*props*/}
+        <ShowUserName name="Igor" />
+        {/*Destructor/desustruturando pops*/}
+        <CarDetails marca="Honda" km={200000} cor="Bege" newCar={false} />
+        {/*reaproveitando componentes */}
+        <CarDetails marca="Ford" km={100000} cor="azul" newCar={false} />
+        <CarDetails marca="fiat" km={0} cor="Branco" newCar={true} />
+        <CarDetails marca="VW" km={0} cor="Prata" newCar={true} />
       </div>
     </div>
   );
---
+++ src/Components/CarDetails.js
@@ -0,0 +1,16 @@
+// desustruturando a props não preciso colocar props.algumaCoisa, a desustruração é feita entre chaves
+const CarDetails = ({ marca, km, cor, newCar }) => {
+  return (
+    <div>
+      <h2>Lista de Carros</h2>
+      <ul>
+        <li>marca: {marca}</li>
+        <li>km: {km}</li>
+        <li>cor:{cor}</li>
+      </ul>
+      {newCar ? <p>Este carro é novo</p> : <p>Este carro é usado</p>}
+    </div>
+  );
+};
+
+export default CarDetails;
